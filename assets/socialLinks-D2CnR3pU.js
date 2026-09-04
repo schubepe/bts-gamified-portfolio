e69271897777d39b0f1904436777035713a56ae1
Schubert <schubepe@amazon.es>
2026-09-04 18:56:27 +0200

    Updates

--- assets/socialLinks-D2CnR3pU.js
+++
@@ -1,1 +0,0 @@
-import{p as i}from"./index-CTnx2l1N.js";const n=(e,d)=>(e==null?void 0:e.trim())||d,s={github:n(void 0,i.github),linkedin:n(void 0,i.linkedin),email:n(void 0,i.email),display:{github:n(void 0,i.github).replace("https://",""),linkedin:n(void 0,i.linkedin).replace("https://",""),email:n(void 0,i.email)}};export{s};
